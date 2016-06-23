225bc5e5ba6a060e4b9a0b87de330f1464a6dbe5
Christian Mansour <c.mansour@miami.edu>
2016-06-23 11:50:31 -0500

    routes set up

---
+++ models/growls.js
@@ -0,0 +1,19 @@
+function Growls () {
+
+this.text = text;
+Growls.all.push(this);
+
+};
+
+
+Growls.all = [];
+
+
+
+
+
+
+
+
+
+module.exports = Growls;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session')
+var Growls = require('../models/growls')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Growler' });
 });
+
+router.get('/growls', function(req, res, next) {
+  res.render('growlsPage', { title: 'All Growls' });
+});
+
+
+router.post('/growls', function(req, res, next) {
+  var singleGrowl = req.body.inputGrowl;
+  // new Growls(session.singleGrowl);
+
+
+  console.log(singleGrowl);
+
+
+  res.redirect('/growls');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
